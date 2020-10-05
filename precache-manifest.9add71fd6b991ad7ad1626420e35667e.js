3ee020234a277ba956f9b494dde1633769a796bc
Yuta123456 <yuuta09090530@icloud.com>
2020-10-05 12:42:27 +0900

    Updates

---
+++ precache-manifest.9add71fd6b991ad7ad1626420e35667e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4a4a321c2e86ea480a5502abbeb2ae0",
+    "url": "/svc_tutorial/index.html"
+  },
+  {
+    "revision": "86c47ae49833561c2ee5",
+    "url": "/svc_tutorial/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "283984425dd606847ac6",
+    "url": "/svc_tutorial/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "86c47ae49833561c2ee5",
+    "url": "/svc_tutorial/static/js/2.476a90fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/svc_tutorial/static/js/2.476a90fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "283984425dd606847ac6",
+    "url": "/svc_tutorial/static/js/main.3bedfecc.chunk.js"
+  },
+  {
+    "revision": "fe696ff68082c78ebe00",
+    "url": "/svc_tutorial/static/js/runtime-main.4a7d0658.js"
+  }
+]);
